33f4af3afb29c54264b0d3bf2b56ea45813c7a3b
wb_renhan <wb_renhan@meituan.com>
2026-09-02 22:14:42 +0800

    更新域名解析

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://echocode.com.cn',
   legacy: {
     collectionsBackwardsCompat: true,
   },
